6fa103b26a37763352dbf4cafb77543018ec3329
HunterJerochim <hunter.jerochim@gmail.com>
2024-06-03 15:12:55 -0700

    First Commit

---
+++ src/components/Main/main.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./main.css";
+import { Link, useLocation } from "react-router-dom";
+
+const Main = () => {
+    const { pathname } = useLocation()
+    const isAboutRoute = pathname === "/about";
+    const isVansRoute = pathname === "/vans";
+    
+    return (
+        <div className="main-page-container">
+            <div className="main-page-inner-container">
+            <div className="nav-container">
+                <Link className="nav-title" to="/">#VANLIFE</Link>
+                <div className="nav-button-container">
+                    <Link 
+                        className={isAboutRoute ? "nav-button-true" : "nav-button"}
+                        to="/about"
+                    >
+                        About
+                    </Link>
+                    <Link 
+                        className="nav-button" 
+                        to="/vans"
+                    >
+                        Vans
+                    </Link>
+                </div>
+            </div>
+            <div className="image-container">
+                <img 
+                    src="https://res.cloudinary.com/dav8yugzm/image/upload/v1716924933/Mountain_vanlife_mbfbg7.png"
+                    className="mountain-image"
+                />
+                <div className="image-overlay">
+                    <div className="image-overlay-text-container">
+                        <h1 className="image-overlay-title">You got the travel plans, we got the travel vans.</h1>
+                        <h2 className="image-overlay-text">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</h2>
+                    </div>
+                    <button className="image-overlay-button">Find your van</button>
+                </div>
+            </div>
+            <div className="footer-container">
+                <h2 className="footer-text">Ⓒ 2022 #VANLIFE</h2>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main;
+
